9a8be3496b69673d0c04f5b434a883dd91c803b4
AdrianaSil <96041964+AdrianaSil@users.noreply.github.com>
2024-02-25 18:20:37 -0300

    Add files via upload

---
+++ RPC_2/cliente.py
@@ -0,0 +1,11 @@
+import xmlrpc.client
+import socket
+
+server = xmlrpc.client.ServerProxy('http://10.0.84.199:22222')
+
+server.salvar("Teste")
+print(server.getMsgs())
+print(server.getServerIP())
+print(server.getServerDateTime())
+
+print(server.system.listMethods())
---
+++ RPC_2/servidor.py
@@ -0,0 +1,38 @@
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+from datetime import datetime
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+with SimpleXMLRPCServer(('10.0.84.199', 22222), requestHandler=RequestHandler) as server:
+    server.register_introspection_functions()
+
+    mensagens = []
+
+    def getServerIP():
+        return server.server_address[0]
+
+    def salvar(msg):
+        mensagens.append(msg)
+        return True
+
+    def getMsgs():
+        return mensagens
+
+    def getServerDateTime():
+        now = datetime.now()
+        formatted_date_time = now.strftime("%Y-%m-%d %H:%M")
+        return formatted_date_time
+
+    def clearMsg():
+        mensagens.clear()
+        return True
+
+    server.register_function(getServerIP, 'Get Server IP')
+    server.register_function(salvar, 'Salvar msg')
+    server.register_function(getMsgs, 'Get msg')
+    server.register_function(getServerDateTime, 'Get DateTime')
+    server.register_function(clearMsg, 'Clear msgs')
+
+    server.serve_forever()
